212b479a6e3f4c5a191e0311f5fe2446d9448323
麋鹿 <1669277128@qq.com>
2019-08-04 16:47:47 +0800

    创建app服务器，使用router进行路由管理

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 //引入中间键  express
 const express = require('express');
+const router = require('router');
 //创建服务器
 const app = express();
-//
+//添加端口监听
+app.listen(8282, () => {
+    console.log('http://127.0.0.1:8282')
+})
+
+//托管静态资源
+app.use('/assets', express.staitic('assets'));
+app.use('/uploads', express.static('upload'));
+
+//配置ejs模板引擎
+app.set('view engine', 'ejs');
+//进行ejs模板文件查询的默认目录配置
+app.set('view', __dirname + '/views');
+
+//让app服务器使用Router进行路由管理
+app.use('router');
