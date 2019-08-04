57e1def35b1e8e6a2e1b68885d63a79a84f02edf
麋鹿 <1669277128@qq.com>
2019-08-04 21:44:27 +0800

    静态资源托管，使用ejs分离公共页面结构，实现服务器渲染浏览器结构

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //引入中间键  express
 const express = require('express');
-const router = require('router');
+const router = require('./router.js');
 //创建服务器
 const app = express();
 //添加端口监听
@@ -9,13 +9,13 @@ app.listen(8282, () => {
 })
 
 //托管静态资源
-app.use('/assets', express.staitic('assets'));
-app.use('/uploads', express.static('upload'));
+app.use('/assets', express.static('assets'));
+app.use('/uploads', express.static('uploads'));
 
 //配置ejs模板引擎
 app.set('view engine', 'ejs');
 //进行ejs模板文件查询的默认目录配置
-app.set('view', __dirname + '/views');
+app.set('views', __dirname + '/views');
 
 //让app服务器使用Router进行路由管理
-app.use('router');
+app.use(router);
---
+++ router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+//引入页面返回控制器
+const pagesController = require('./controllers/pagesController.js')
+const userController = require('./controllers/userController.js')
+let router = express.Router();
+
+//配置路由
+//后台页面
+router.get('/admin', pagesController.getAdminIndexPage)
+    .get('/admin/categories', pagesController.getAdminCategoriesPage)
+    .get('/admin/comments', pagesController.getAdminCommentsPage)
+    .get('/admin/login', pagesController.getAdminLoginPage)
+    .get('/admin/navmenus', pagesController.getAdminNavMenusPage)
+    .get('/admin/passwordreset', pagesController.getAdminPasswordResetPage)
+    .get('/admin/postadd', pagesController.getAdminPostAddPage)
+    .get('/admin/posts', pagesController.getAdminPostsPage)
+    .get('/admin/profile', pagesController.getAdminProfilePage)
+    .get('/admin/settings', pagesController.getAdminSettingsPage)
+    .get('/admin/sildes', pagesController.getAdminSlidesPage)
+    .get('/admin/users', pagesController.getAdminUsersPage)
+    //前台页面
+    .get('/index', pagesController.getIndexPage)
+    .get('/detail', pagesController.getDetailPage)
+    .get('/list', pagesController.getListPage)
+module.exports = router 
